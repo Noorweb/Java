9be840b9e62497ebf67fc09200cf62c1446e31dc
Noorweb <86409864+Noorweb@users.noreply.github.com>
2021-09-30 18:38:50 +0300

    Add files via upload

---
+++ EX2.java
@@ -0,0 +1,13 @@
+J/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Noor
+ */
+public class EX2 {
+    
+}
